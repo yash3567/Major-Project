e075b12f6f0923d463161b961105e571ba333455
yash3567 <yashnandeshwar32@gmail.com>
2025-03-23 12:33:38 +0530

    Solve the Axios Error from ProjectControllers-File
    Signed-off-by: yash3567 <yashnandeshwar32@gmail.com>

--- backend/controllers/project-controller.js
+++ backend/controllers/project-controller.js
@@ -43,17 +43,6 @@ export const handleProjects = async (req, res) => {
       projectImage: projectImageUpload.secure_url, // Save Cloudinary image URL
     });
 
-    // ✅ Verify user and password
-    const userData = await usersPassword.findOne({ email });
-    if (!userData) {
-      return res.status(404).json({ success: false, message: "User not found with the provided email" });
-    }
-
-    const passwordVerify = await checkHashedPassword(password, userData.password);
-    if (!passwordVerify) {
-      return res.json({ success: false, message: "Password Not Verified, Please Check Again" });
-    }
-
     res.json({
       success: true,
       message: "Project Uploaded Successfully",
